e32a346e4eb302a547cb64d20911be1a8466d989
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 17:19:47 -0300

    x

--- Screens/Categories.js
+++ Screens/Categories.js
@@ -17,16 +17,15 @@ const Categories = (props) => {
     console.log('categories')
   },[])
 
-
-  const categories = [{name: 'Accesorios', pic: 'https://cdn.evance.me/portal/web/296/content/images/guitar-picks/tortex-guitarr-picks.png'},
+  /*const categories = [{name: 'Accesorios', pic: 'https://cdn.evance.me/portal/web/296/content/images/guitar-picks/tortex-guitarr-picks.png'},
   {name: 'Amplificadores', pic: 'https://www.wallpaperuse.com/wallp/9-99781_m.jpg'},
   {name: 'Bajos', pic: 'https://cdn.shopify.com/s/files/1/1413/0250/products/yamaha-trbx505tbl-bass-guitar-trans-black_666x518.jpg?v=1555294252'},
   {name: 'Guitarras', pic: 'https://www.gearank.com/sites/default/files/styles/large/public/field/image/Types-of-Acoustic-Guitars.jpg?itok=IITs3aFo'},
   {name: 'Pedales y pedaleras', pic: 'https://i0.wp.com/100lecciones.org/wp-content/uploads/pedal-de-guitarra-2.jpg?fit=800%2C443&ssl=1'}, 
   {name: 'Percusión', pic: 'https://www.worldofmusic.com.au/wp-content/uploads/2019/12/Pearl-Roadshow-Junior-Jet-Black.jpg'}, 
   {name: 'Teclados', pic: 'https://http2.mlstatic.com/D_NQ_NP_768004-MLA41768648746_052020-O.jpg'}, 
-  { name: 'Sonido', pic: 'https://files.soniccdn.com/files/2015/07/02/dlive-s3000.png'}];
-  
+  {name: 'Sonido', pic: 'https://files.soniccdn.com/files/2015/07/02/dlive-s3000.png'}];
+  */
   
   props.navigation.setOptions({
     title: 'Categorías',
@@ -58,25 +57,6 @@ const Categories = (props) => {
       <View style={{padding: 30, alignItems:'center'}}>
         <Text style={{color: 'white', fontSize: 20}}>Elegí la categoría de tu instrumento</Text>
       </View>
-       <FlatList
-       style={{backgroundColor: 'rgb(16, 16, 16)'}}
-        data={categories}
-        keyExtractor={(data, index) => index.toString()}
-        renderItem={(data, rowMap) => (
-          <TouchableWithoutFeedback
-            onPress={() => {
-             props.navigation.navigate('ProductsByCategory',{category:data.item.name, allProducts: allProducts})
-              ;
-            }}>
-            <View style={{backgroundColor: '#f1f3f6', paddingVertical: 10, paddingHorizontal: 10, marginVertical: 4, borderRadius: 4, display: 'flex', flexDirection: 'row',  alignItems: 'center' }} >
-              <Image source={{uri: data.item.pic}} style={{width: 60, height: 60, resizeMode: 'cover', borderRadius: 60}}/>
-              <Text style={{marginLeft: 10, fontWeight: '400', fontSize: 16, paddingVertical: 10 }}>
-                {data.item.name}
-              </Text>
-            </View>
-          </TouchableWithoutFeedback>
-        )}
-      />
     </View>
   )
 }
@@ -101,9 +81,7 @@ const mapDispatchToProps = {
 const mapStateToProps = state =>{
   return{
     shoppingCart:state.shopping.shoppingCart,
-    allProducts: state.product.allProducts,
-    categories:state.product.categories
-
+    allProducts: state.product.allProducts
    }
  }
  export default connect(mapStateToProps,mapDispatchToProps)(Categories)
